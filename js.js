f521bbf96e536a53f1608fc69d960cd553ba0134
Ansh Sharma <146317538+Ansh-2222@users.noreply.github.com>
2024-09-13 02:11:00 +0530

    updated code

--- js.js
+++ js.js
@@ -5,3 +5,10 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+var typed = new Typed(".atext ", {
+    strings: ["Programmer", "Frontend Developer", "Gamer", "Video Editor"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
